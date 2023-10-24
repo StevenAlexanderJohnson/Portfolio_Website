bede70f9d9ada43547be3c1623e46c719c7716c5
Steven Johnson <steveajohn1@gmail.com>
2023-10-24 19:18:31 -0400

    trying to fix route

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({command}) => {
   return {
     plugins: [react()],
-    base: command === 'serve' ? '' : '/vite-react-swc/',
+    base: command === 'serve' ? '' : '/Portfolio_Website/',
   }
 })
