14ebd28356650aa39b235ab8becb743ee36280a6
Steven Johnson <steveajohn1@gmail.com>
2023-10-24 19:25:43 -0400

    changed links to navigate

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Fragment } from "react";
 import { useAuth0 } from '@auth0/auth0-react';
 
 export default function NavbarWrapper() {
     const { loginWithRedirect, isAuthenticated, logout, isLoading } = useAuth0();
     console.log(isAuthenticated)
+    const navigate = useNavigate();
 
     const authButton = isAuthenticated == true
         ? <button className="hover:text-accent-light dark:hover:text-accent-dark transition-colors duration-100" onClick={() => logout()}>Logout</button>
@@ -13,14 +14,29 @@ export default function NavbarWrapper() {
     return (
         <Fragment>
             <div className="flex justify-between px-5 pt-3">
-                <a href="/" className="hover:text-accent-light dark:hover:text-accent-dark transition-colors duration-100">Steven Johnson</a>
+                <button className="hover:text-accent-light dark:hover:text-accent-dark transition-colors duration-100" onClick={() => {
+                    navigate('/');
+                }}>
+                    Steven Johnson
+                </button>
                 <div className="flex justify-between flex-col sm:flex-row gap-1 sm:gap-10">
                     <button className="hover:text-accent-light dark:hover:text-accent-dark transition-colors duration-100" onClick={() => {
+                        navigate('/');
                         document.getElementById("about")?.scrollIntoView({ behavior: "smooth", block: "end" });
-                    }}>About</button>
-                    <a href="/projects" className="hover:text-accent-light dark:hover:text-accent-dark transition-colors duration-100">Projects</a>
-                    <a href="/contact" className="hover:text-accent-light dark:hover:text-accent-dark transition-colors duration-100">Contact</a>
-                    {isLoading ? <div>loading...</div> : authButton }
+                    }}>
+                        About
+                    </button>
+                    <button className="hover:text-accent-light dark:hover:text-accent-dark transition-colors duration-100" onClick={() => {
+                        navigate('/projects');
+                    }}>
+                        Projects
+                    </button>
+                    <button className="hover:text-accent-light dark:hover:text-accent-dark transition-colors duration-100" onClick={() => {
+                        navigate('/contact');
+                    }}>
+                        Contact
+                    </button>
+                    {isLoading ? <div>loading...</div> : authButton}
                 </div>
             </div>
             <Outlet />
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({command}) => {
   return {
     plugins: [react()],
-    base: '/Portfolio_Website/',
+    base: '/',
   }
 })
