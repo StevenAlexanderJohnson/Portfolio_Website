22309a7e177a2eceed6263e8a0b675dc5751c80a
Steven Johnson <steveajohn1@gmail.com>
2023-10-24 19:16:54 -0400

    trying to fix route

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
             }
         ]
     }
-]);
+], {basename: import.meta.env.DEV ? '/' : '/Portfolio_Website'});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Portfolio_Website/',
-  plugins: [react()],
+export default defineConfig(({command}) => {
+  return {
+    plugins: [react()],
+    base: command === 'serve' ? '' : '/vite-react-swc/',
+  }
 })
